225dc884111579cbd68ebdf941db4be9d551aedd
Assad zakir <assadzakirhussain@gmail.com>
2017-04-11 08:01:19 +0500

    Added header color in home screen

--- meetups/src/containers/home/homeScreen.js
+++ meetups/src/containers/home/homeScreen.js
@@ -3,25 +3,29 @@ import { View, Text, ActivityIndicator } from 'react-native';
 import { MeetupApi } from '../../../constants/api';
 import loadingScreen from '../../shared';
 import MeetupList from '../../components/meetupList/meetupList';
+import Colors from '../../../constants/colors';
 import { FontAwesome, MaterialIcons } from '@expo/vector-icons';
 import styles from './style';
 
 const meetupApi = new MeetupApi();
 
 class homeScreen extends Component {
-  static navigationOptions = {
-    tabBar: {
-      icon: ({ tintColor }) => (
-        <FontAwesome
-          name="home"
-          size={25}
-          color={tintColor}
-        />
-      ),
-    },
-  }
+    static navigationOptions = {
+        header: {
+            style: { backgroundColor: Colors.redColor },
+        },
+        tabBar: {
+            icon: ({ tintColor }) => (
+                <FontAwesome
+                    name="home"
+                    size={25}
+                    color={tintColor}
+                />
+            ),
+        },
+    }
 
-  
+
     static defaultProps = {
         meetupApi
     }
