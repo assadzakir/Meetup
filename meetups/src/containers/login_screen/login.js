7590285e07df074770beeff9d986bc4add02937b
Assad zakir <assadzakirhussain@gmail.com>
2017-05-20 20:16:44 +0500

    Implemented client facebook auth

--- meetups/src/containers/login_screen/login.js
+++ meetups/src/containers/login_screen/login.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Alert } from 'react-native';
+import { Facebook } from 'expo';
 import Colors from '../../../constants/colors';
+import fbConfig from '../../../constants/fbConfig';
 import styled from 'styled-components/native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import styles from './style';
 
 
@@ -33,6 +36,34 @@ const Button = styled.TouchableOpacity`
 `;
 
 class LoginScreen extends Component {
+
+    _onLoginPress = provider => {
+        if (provider == 'facebook') {
+            this._loginWithFacebook();
+        } else {
+            this._loginWithGoogle();
+        }
+    }
+
+    async _loginWithFacebook() {
+        const {
+        type,
+            token,
+    } = await Facebook.logInWithReadPermissionsAsync(fbConfig.APP_ID, {
+                permissions: ['public_profile', 'email'],
+            });
+
+        if (type === 'success') {
+            // Get the user's name using Facebook's Graph API
+            const response = await fetch(
+                `https://graph.facebook.com/me?access_token=${token}`);
+            Alert.alert(
+                'Logged in!',
+                `Hi ${(await response.json()).name}!`,
+            );
+        }
+    }
+
     render() {
         return (
             <FlexContainer>
@@ -47,9 +78,7 @@ class LoginScreen extends Component {
                         <FlexContainer>
                             <Text style={styles.authWelcomeText}>
                                 Start managing your
-                {' '}
                                 <MeetupText>Appointment</MeetupText>
-                                {' '}
                                 quickly and efficently
               </Text>
                         </FlexContainer>
@@ -59,10 +88,15 @@ class LoginScreen extends Component {
                             color="#db3236"
                         >
                             <Text style={styles.buttonAuth}>Sign with Google</Text>
+                            <MaterialCommunityIcons name='google' size={30}
+                                color='#fff' />
                         </Button>
                         <Button
-                            color="#3b5998">
+                            color="#3b5998"
+                            onPress={() => this._onLoginPress('facebook')}>
                             <Text style={styles.buttonAuth}>Sign with Facebook</Text>
+                            <MaterialCommunityIcons name='facebook' size={30}
+                                color='#fff' />
                         </Button>
                     </BottomButtonWrapper>
                 </FlexContainer>
